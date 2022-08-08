9aabb4889173e72fcd0e605bf6217115888c8871
胡伟涛 <50671163@qq.com>
2022-08-08 15:35:11 +0800

    更新web3架构文件

--- src/Tool/Validator.php
+++ src/Tool/Validator.php
@@ -32,4 +32,35 @@ class Validator
     {
         return (preg_match('/^0x[a-fA-F0-9]{64}$/', $value) >= 1);
     }
+
+    /**
+     * 数量验证
+     * @param  string  $value
+     * @return bool
+     */
+    public static function quantity(string $value): bool
+    {
+        return is_numeric($value) || preg_match('/^0x[a-fA-F0-9]*$/', $value) >= 1;
+    }
+
+    /**
+     * 标签验证
+     * @param  string  $value
+     * @return bool
+     */
+    public static function tag(string $value): bool
+    {
+        return in_array($value, ['latest', 'earliest', 'pending'], true);
+    }
+
+    /**
+     * 随机数验证
+     *
+     * @param  string  $value
+     * @return bool
+     */
+    public static function nonce(string $value): bool
+    {
+        return (preg_match('/^0x[a-fA-F0-9]{16}$/', $value) >= 1);
+    }
 }
